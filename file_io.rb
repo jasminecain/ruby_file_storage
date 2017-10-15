e75c24d54da135ed2e4700b27e274cf2f3af68fa
Jasmine Quach <xjasminequach@gmail.com>
2017-10-14 21:45:57 -0500

    Complete exercise

---
+++ file_io.rb
@@ -0,0 +1,34 @@
+require 'YAML'
+
+makes = File.open('car_makes.txt', 'r+')
+models = File.open('car_models.txt', 'r+')
+
+# puts makes.read
+# puts models.read
+
+hash = Hash.new
+
+makesArr = []
+modelsArr = []
+modelOnly = []
+makesOnly = []
+
+# splits models into array of arrays
+models.each do |i|
+  modelsArr = i.split('=')
+  # puts modelsArr[1]
+  modelOnly << modelsArr[1].chomp
+end
+# puts modelOnly
+
+makes.each do |x|
+  makesArr = x.chomp
+  # puts makesArr
+  # creates a hash; makes become the keys and models the values
+  makesOnly << x.chomp
+end
+# puts makesOnly
+
+# chomp gets rid of extra characters
+hash = Hash[makesOnly.zip modelOnly]
+puts hash
